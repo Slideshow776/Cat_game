4d93b877c61c692d0fdcbecbdc239983e87f64f9
Sandra Moen <sandra77_6@hotmail.com>
2018-08-04 20:44:43 +0200

    fixed map layer rendering + impassable objects

--- core/src/com/sandra/game/screens/Level_1_1.java
+++ core/src/com/sandra/game/screens/Level_1_1.java
@@ -6,15 +6,22 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.DelayedRemovalArray;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -55,17 +62,20 @@ public class Level_1_1 implements Screen {
     private DelayedRemovalArray<Entity> holes;
 
     private Controls controls;
-   
+
     private TiledMap map;
     private OrthoCachedTiledMapRenderer mapRenderer;
     private FitViewport viewport;
 
-	private Stage stage;
-    
+    private Stage stage;
+
+    private int[] backgroundLayers = { 0, 1 }; // don't allocate every frame!
+    private int[] foregroundLayers = { 2 }; // don't allocate every frame!
+
     public Level_1_1(Cat_game game) {
         this.game = game;
         pause = false;
-        
+
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Constants.GAME_WIDTH / Constants.PPM, Constants.GAME_HEIGHT / Constants.PPM);
         game.batch.setProjectionMatrix(camera.combined);
@@ -74,27 +84,32 @@ public class Level_1_1 implements Screen {
         b2d_world = new World(new Vector2(0, 0), true);
         myContactListener = new MyContactListener();
         b2d_world.setContactListener(myContactListener);
-        if (Constants.B2D_DEBUGGING) {b2d_Renderer = new Box2DDebugRenderer();}
+        if (Constants.B2D_DEBUGGING) {
+            b2d_Renderer = new Box2DDebugRenderer();
+        }
         box2d = new box2D(b2d_world);
         box2d.set_world_bounds();
 
         AssetManager assetManager = new AssetManager();
         Assets.instance.init(assetManager);
 
-        //sounds
+        // sounds
         score1 = Gdx.audio.newSound(Gdx.files.internal("sounds/score1.wav"));
         coin1 = Gdx.audio.newSound(Gdx.files.internal("sounds/coin1.wav"));
         purr1 = Gdx.audio.newSound(Gdx.files.internal("sounds/purr1.wav"));
         bump1 = Gdx.audio.newSound(Gdx.files.internal("sounds/yarn_ball_bump1.wav"));
 
-        //music
+        // music
         generic_music = Gdx.audio.newMusic(Gdx.files.internal("music/video-game-7.wav"));
         generic_music.setLooping(true);
         generic_music.setVolume(.1f);
 
         // map
-        map = new TmxMapLoader().load("levels/test.tmx");
-        mapRenderer = new OrthoCachedTiledMapRenderer(map, 1 / Constants.PPM);
+        map = new TmxMapLoader().load("levels/level1-1.tmx");
+        mapRenderer = new OrthoCachedTiledMapRenderer(map, 1 / Constants.PPM, 8191); // 8191 is max
+        mapRenderer.setView(camera);
+
+        create_world_boundaries();
 
         // Entities
         cat1s = new DelayedRemovalArray<Entity>();
@@ -103,7 +118,7 @@ public class Level_1_1 implements Screen {
         coins = new DelayedRemovalArray<Entity>();
         populate_entity_from_map("coins", coins);
 
-        yarn_balls = new DelayedRemovalArray<Entity>();        
+        yarn_balls = new DelayedRemovalArray<Entity>();
         populate_entity_from_map("yarn_balls", yarn_balls);
 
         holes = new DelayedRemovalArray<Entity>();
@@ -115,30 +130,62 @@ public class Level_1_1 implements Screen {
         controls = new Controls(entities);
     }
 
-    private void populate_entity_from_map(String entity, DelayedRemovalArray<Entity> entity_list) {
-        for(MapObject object : map.getLayers().get(entity).getObjects().getByType(RectangleMapObject.class)) {
-            Vector2 position = new Vector2(
-                ((RectangleMapObject) object).getRectangle().getX() / Constants.PPM,
-                ((RectangleMapObject) object).getRectangle().getY() / Constants.PPM
+    private void create_world_boundaries() {
+        for (MapObject object : map.getLayers().get("impassable").getObjects().getByType(RectangleMapObject.class)) {
+            Vector2 position = new Vector2(((RectangleMapObject) object).getRectangle().getX() / Constants.PPM,
+                    ((RectangleMapObject) object).getRectangle().getY() / Constants.PPM);
+            float width = ((RectangleMapObject) object).getRectangle().getWidth() / Constants.PPM;
+            float height = ((RectangleMapObject) object).getRectangle().getWidth() / Constants.PPM;
+
+            BodyDef bdef = new BodyDef();
+            bdef.type = BodyType.StaticBody;
+            bdef.position.set(
+                position.x + width / 2,
+                position.y + height / 2
             );
-            if (entity == "cats") entity_list.add(new Cat1(position, b2d_world));
-            if (entity == "coins") entity_list.add(new Coin(position, b2d_world));
-            if (entity == "yarn_balls") entity_list.add(new Yarn_Ball(position, b2d_world));
-            if (entity == "holes") entity_list.add(new Hole(position, b2d_world));
+
+            PolygonShape shape = new PolygonShape();
+            shape.setAsBox(width / 2, height / 2);
+            FixtureDef fdef = new FixtureDef();
+            fdef.shape = shape;
+            fdef.filter.categoryBits = Constants.B2D_BIT_WORLD;
+            fdef.filter.maskBits = Constants.B2D_BIT_CAT1S | Constants.B2D_YARN_BALLS;
+
+            Body world_lower = b2d_world.createBody(bdef);
+            world_lower.createFixture(fdef).setUserData(Constants.B2D_IDENTITY);
+        }
+    }
+
+    private void populate_entity_from_map(String entity, DelayedRemovalArray<Entity> entity_list) {
+        for (MapObject object : map.getLayers().get(entity).getObjects().getByType(RectangleMapObject.class)) {
+            Vector2 position = new Vector2(((RectangleMapObject) object).getRectangle().getX() / Constants.PPM,
+                    ((RectangleMapObject) object).getRectangle().getY() / Constants.PPM);
+            if (entity == "cats")
+                entity_list.add(new Cat1(position, b2d_world));
+            else if (entity == "coins")
+                entity_list.add(new Coin(position, b2d_world));
+            else if (entity == "yarn_balls")
+                entity_list.add(new Yarn_Ball(position, b2d_world));
+            else if (entity == "holes")
+                entity_list.add(new Hole(position, b2d_world));
+            else if (entity == "impassable") {
+                System.out.println("dsada");
+            }
         }
     }
 
     private void update(float delta) {
-        camera.update();       
+        camera.update();
         b2d_world.step(Constants.B2D_TIMESTEP, Constants.B2D_VELOCITY_ITERATIONS, Constants.B2D_POSITION_ITERATIONS);
         controls.update(delta);
         game.batch.setProjectionMatrix(camera.combined);
-        mapRenderer.setView(camera);
 
         // entities
-        for (Entity hole:holes) {hole.update(delta);}
+        for (Entity hole : holes) {
+            hole.update(delta);
+        }
 
-        for (Entity cat1:cat1s) {
+        for (Entity cat1 : cat1s) {
             if (cat1.get_body().getUserData() == "win_condition") {
                 score1.play();
                 cat1.dispose();
@@ -147,7 +194,7 @@ public class Level_1_1 implements Screen {
             cat1.update(delta);
         }
 
-        for (Entity coin:coins) {
+        for (Entity coin : coins) {
             if (coin.get_body().getUserData() == "remove_me") {
                 coin1.play();
                 coin.dispose();
@@ -156,7 +203,7 @@ public class Level_1_1 implements Screen {
             coin.update(delta);
         }
 
-        for (Entity yarn_ball:yarn_balls) {
+        for (Entity yarn_ball : yarn_balls) {
             if (yarn_ball.get_body().getUserData() == "collision") {
                 bump1.play();
             }
@@ -165,38 +212,48 @@ public class Level_1_1 implements Screen {
     }
 
     @Override
-    public void render(float delta) {	
-        if (!pause) {update(delta);}
-        
-        Gdx.gl.glClearColor(0, 0, 0, 1); // black
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);        
-        
-        mapRenderer.render();
+    public void render(float delta) {
 
-        game.batch.begin();        
-        for (Entity yarn_ball: yarn_balls) yarn_ball.render(game.batch);
-        for (Entity cat1: cat1s) cat1.render(game.batch);
-        for (Entity coin: coins) coin.render(game.batch);
-        for(Entity hole: holes) hole.render(game.batch);
+        Gdx.gl.glClearColor(0, 0, 0, 1); // black
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        if (!pause)
+            update(delta);
+
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        mapRenderer.render(backgroundLayers);
+        Gdx.gl.glDisable(GL20.GL_BLEND);
+
+        game.batch.begin();
+        for (Entity yarn_ball : yarn_balls)
+            yarn_ball.render(game.batch);
+        for (Entity cat1 : cat1s)
+            cat1.render(game.batch);
+        for (Entity coin : coins)
+            coin.render(game.batch);
+        for (Entity hole : holes)
+            hole.render(game.batch);
         game.batch.end();
-        
-        if (Constants.B2D_DEBUGGING) b2d_Renderer.render(b2d_world, camera.combined);
+
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        mapRenderer.render(foregroundLayers);
+        Gdx.gl.glDisable(GL20.GL_BLEND);
+
+        if (Constants.B2D_DEBUGGING)
+            b2d_Renderer.render(b2d_world, camera.combined);
     }
 
     @Override
     public void resize(int width, int height) {
-        /* camera.setToOrtho(
-            false,
-            (Constants.GAME_WIDTH) / Constants.PPM,
-            (Constants.GAME_HEIGHT) / Constants.PPM
-        );
-        game.batch.setProjectionMatrix(camera.combined); */
+        camera.setToOrtho(false, (Constants.GAME_WIDTH) / Constants.PPM, (Constants.GAME_HEIGHT) / Constants.PPM);
+        game.batch.setProjectionMatrix(camera.combined);
     }
 
     @Override
     public void pause() {
         pause = !pause;
-        generic_music.pause();        
+        generic_music.pause();
     }
 
     @Override
@@ -206,27 +263,33 @@ public class Level_1_1 implements Screen {
     }
 
     @Override
-    public void hide() {dispose();}
-
-	@Override
-	public void show() {
-        purr1.play();
-        generic_music.play();		
+    public void hide() {
+        dispose();
     }
 
     @Override
-    public void dispose () {
+    public void show() {
+        purr1.play();
+        generic_music.play();
+    }
+
+    @Override
+    public void dispose() {
         Assets.instance.dispose();
-        for (Entity cat1:cat1s) {
+        for (Entity cat1 : cat1s) {
             controls.removeEntity(cat1.getId());
             cat1.dispose();
         }
-        for(Entity hole: holes) hole.dispose();
-        for (Entity coin: coins) coin.dispose();
-        for (Entity yarn_ball: yarn_balls) yarn_ball.dispose();
+        for (Entity hole : holes)
+            hole.dispose();
+        for (Entity coin : coins)
+            coin.dispose();
+        for (Entity yarn_ball : yarn_balls)
+            yarn_ball.dispose();
         generic_music.dispose();
         purr1.dispose();
         b2d_world.dispose();
         b2d_Renderer.dispose();
-    }    
+        mapRenderer.dispose();
+    }
 }
