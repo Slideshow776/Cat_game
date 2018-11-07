6c62bc82d6e02a995006e41b5422e59243ca2ef6
Sandra Moen <sandra77_6@hotmail.com>
2018-11-07 07:21:51 +0100

    added two new levels

---
+++ core/src/com/sandra/game/screens/Level_2_3.java
@@ -0,0 +1,13 @@
+package com.sandra.game.screens;
+
+import com.sandra.game.Cat_game;
+
+public class Level_2_3 extends Level {
+
+    public static final String TAG = Level_2_2.class.getName();
+    private static final String level = "levels/level2-3.tmx";
+
+    public Level_2_3(Cat_game game) {
+        super(game, level);
+    }
+}
