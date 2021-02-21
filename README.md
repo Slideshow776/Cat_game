42c95c8fbe3b2091b74f2f10cecc340c618d5f98
Sandra Moen <sandramoen01@gmail.com>
2021-02-21 11:23:19 +0100

    reupload version to GPC

--- README.md
+++
@@ -1,9 +0,0 @@
-# Cat_game
-Creating an Android Game with libGDX
-
-[Google Play Store](https://play.google.com/store/apps/details?id=com.sandra.game&hl=en)
-
-[Youtube trailer](https://www.youtube.com/watch?v=L1S8Rg2kjgU)
-
-![catgame](https://user-images.githubusercontent.com/4059636/52280544-7220e800-295c-11e9-8a4f-c07eac1ec213.PNG)
-
---
+++ android/src/no/sandramoen/catgame/AndroidLauncher.java
@@ -0,0 +1,15 @@
+package no.sandramoen.catgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new com.sandra.game.Cat_game(), config);
+	}
+}
